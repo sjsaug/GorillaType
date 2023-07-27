2dbcf754fb0b5f91f73e8796984603f7a67323ba
sjsaug <sjsaug@gmail.com>
2023-07-27 00:10:32 -0400

    fix wpm

--- typing.js
+++ typing.js
@@ -28,19 +28,19 @@ function newGame(){
     addClass(document.querySelector(".letter"), "current");
 }
 
-function getWPM(){
-    const words = [...document.querySelectorAll(".word")];
-    const lastTypedWord = document.querySelector(".word.current");
-    const lastTypedWordIndex = words.indexOf(lastTypedWord) +1;
+function getWPM() {
+    const words = [...document.querySelectorAll('.word')];
+    const lastTypedWord = document.querySelector('.word.current');
+    const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
     const typedWords = words.slice(0, lastTypedWordIndex);
     const correctWords = typedWords.filter(word => {
-        const letters = [...word.children];
-        const incorrectLetters = letters.filter(letter => letter.className.includes("incorrect"));
-        const correctLetters = letters.filter(letter => letter.className.includes("correct"));
-        return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+      const letters = [...word.children];
+      const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
+      const correctLetters = letters.filter(letter => letter.className.includes('correct'));
+      return incorrectLetters.length === 0 && correctLetters.length === letters.length;
     });
-    return correctWords.lenght / gameTime * 60000;
-}
+    return correctWords.length / gameTime * 60000;
+  }
 
 function gameOver(){
     clearInterval(window.timer);
